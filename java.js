78a8ede8a2de7a33fd53f4d7c2d2bba33d347784
LUSANI MAGADANI <magadanilusani2003@gmail.com>
2025-08-07 15:24:57 +0200

    k
    k

--- java.js
+++ java.js
@@ -5,14 +5,38 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scroll > 500){
+              $('.scroll-up-btn').addClass("show");
+        }
+        else{
+             $('.scroll-up-btn').removeClass("show");
+        }
     });
+
+    // slide-up script 
+      $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop: 0});
+      });
 
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-   
+    // typing Animation script //
+        var typed = new Typed(".typing",{
+            string: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+            typeSpeed: 100,
+            backspeed:60,
+            loop: true
+    });
+
+       var typed = new Typed(".typing-2",{
+            string: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+            typeSpeed: 100,
+            backspeed:60,
+            loop: true
+    });
 
     // owl carousel script
     $('.carousel').owlCarousel({
